e5931254f2a5ac9946015a93d9511fcf1d765098
Ervin543 <ervindasratt@gmail.com>
2023-01-05 15:07:52 -0600

    Created questions.js and added questions and answers to it

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+       numb:'What does "HTTP stand" for ?',
+       options: ['Hyperion Transfer Text Practice', 'Hypno Text Turing Protoge', 'Hyper Text Transfer Protocol', 'How To Train Parrot'],
+       answer:'Hyper Text Transfer Protocol',
+       
+    },
+    {
+        numb:'What Method Outputs a Message to the Web Console ?',
+        options: ['show.message', 'text', 'console.log', 'display.string'],
+        answer:'console.log',
+        
+     },
+     {
+        numb:'What Does "CSS" stand for ?',
+        options: ['Computer Super Scientist', 'Completely Synchronized Swans', 'Cascading Style Sheets', 'Computer Stylist System'],
+        answer:'Cascading Style Sheets',
+        
+     },
+     {
+        numb:'Who Owns JavaScript ?',
+        options: ['Apple Corporation', 'Microsoft Corporation', 'Oracle Corporation', 'Folgers'],
+        answer:'Oracle Corporation',
+        
+     },
+     {
+        numb:'What is this symbol, "($)", Specifically called in JQuery ?',
+        options: ['Dollar Sign', 'Currency Indicator', 'Bling Operator', 'Pound Symbol'],
+        answer:'Bling Operator',
+        
+     },
+     {
+        numb:'What Does "HTML" Stand For ?',
+        options: ['Hyper Tension Machine Language', 'Hyper Text Markup Language', 'Hybrid Time Makeup Length', 'Hungry Tarantulas Make Lunch'],
+        answer:'Hyper Text Markup Language',
+        
+     },
+];
